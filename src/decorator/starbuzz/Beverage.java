a0d95c86483bf7736795df944bbabfe55ba65bc7
NightSkyAsHate <Kazakovgerman@gmail.com>
2022-06-07 11:53:25 +0300

    decorator

---
+++ src/decorator/starbuzz/Beverage.java
@@ -0,0 +1,11 @@
+package decorator.starbuzz;
+
+public abstract class Beverage {
+  String description = "Unknown beverage";
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public abstract double cost();
+}
